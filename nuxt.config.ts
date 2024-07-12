72daf4be07b36a4764e3cd9b9011237f3360c264
ookkamkoo <ookkamkoo@hotmail.com>
2024-07-12 21:02:57 +0700

    first commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,7 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       apiBase: `${process.env.NUXT_PUBLIC_API_BASE}`,
-      apiServer: `${process.env.NUXT_API_PUBLIC_SERVER}`
+      apiServer: "aa"
     }
   },
   hooks: {
