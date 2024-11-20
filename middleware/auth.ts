7a076880ac2bbf7372fdd2d3a8f8a5d12af4efc4
ookkamkoo <ookkamkoo@hotmail.com>
2024-11-21 02:51:44 +0700

    update bank

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -30,7 +30,7 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
                 logout();
                 // Alert("error","กรุณาเข้าสู่ระบบใหม่อีกครั้ง ใหม่อีกครั้ง.")
               }else{
-                  // return navigateTo('/information/withdraw');
+                  return navigateTo('/');
               }
           }
       }
