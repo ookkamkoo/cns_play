7adea36fe32d63f2dc45fca58e686b34e54de4fd
ookkamkoo <ookkamkoo@hotmail.com>
2024-11-21 10:19:28 +0700

    update bank

--- layouts/information.vue
+++ layouts/information.vue
@@ -102,74 +102,77 @@
   };
 </script>
 <style scoped>
-    #components-layout-demo-top-side-2 .logo {
-    float: left;
-    width: 120px;
-    height: 31px;
-    margin: 16px 24px 16px 0;
-    background: rgba(255, 255, 255, 0.3);
-    }
-    .sidebar{
-      flex: 0 0 0 !important;
-      max-width: 350px !important;
-      min-width: 360px !important;
-      width: 400px !important;
-      margin: 3rem 1rem 3rem 3rem;
-    }
+  #components-layout-demo-top-side-2 .logo {
+  float: left;
+  width: 120px;
+  height: 31px;
+  margin: 16px 24px 16px 0;
+  background: rgba(255, 255, 255, 0.3);
+  }
+  .sidebar{
+    flex: 0 0 0 !important;
+    max-width: 350px !important;
+    min-width: 360px !important;
+    width: 400px !important;
+    margin: 3rem 1rem 3rem 3rem;
+  }
 
-    .sidebar,.main-detail{
-    background: none !important;
-    }
+  .sidebar,.main-detail{
+  background: none !important;
+  }
 
-    .main-detail{
-    color: whitesmoke  !important;
-    width: calc(100%-400px) !important;
-    margin: 3rem 3rem 3rem 1rem;
-    }
-    
-    .small-main-detail{
-      margin: 1rem 1rem;
-      /* width: 100% !important; */
-      /* width: calc(100% - 400px) !important; */
-    }
-    .small-header{
-    padding: 0 0.5rem;
-    }
+  .main-detail{
+  color: whitesmoke  !important;
+  width: calc(100%-400px) !important;
+  margin: 3rem 3rem 3rem 1rem;
+  }
+  
+  .small-main-detail{
+    margin: 1rem 1rem;
+    /* width: 100% !important; */
+    /* width: calc(100% - 400px) !important; */
+  }
+  .small-header{
+  padding: 0 0.5rem;
+  }
 
-    .ant-row-rtl #components-layout-demo-top-side-2 .logo {
-    float: right;
-    margin: 16px 0 16px 24px;
-    }
+  .ant-row-rtl #components-layout-demo-top-side-2 .logo {
+  float: right;
+  margin: 16px 0 16px 24px;
+  }
 
-    .site-layout-background {
-    background: #fff;
-    }
+  .site-layout-background {
+  background: #fff;
+  }
 
-    .header{
-    height: 100px !important;
-    position: sticky;
-    top: 0;
-    left: 0;
-    right: 0;
-    width: 100%;
-    z-index: 999;
-    }
-    .main{
-    /* background: url(https://image.1million.social/image/imageList/1707499900431.jpg); */
-    background-repeat: repeat;
-    background-attachment: fixed;
-    background-position: center;
-    min-height: 100vh;
-    }
-    .info-main{
-      background-color: rgba(0, 0, 0, 0.739);
-      border-radius: 10px;
-      padding: 1rem;
-      color: whitesmoke;
-    }
-    .info-main-detail{
-      background-color: rgba(0, 0, 0, 0.739) !important;
-      border-radius: 10px;
-      padding: 1rem;
-    }
+  .header{
+  height: 100px !important;
+  position: sticky;
+  top: 0;
+  left: 0;
+  right: 0;
+  width: 100%;
+  z-index: 999;
+  }
+  .main{
+  /* background: url(https://image.1million.social/image/imageList/1707499900431.jpg); */
+  background-repeat: repeat;
+  background-attachment: fixed;
+  background-position: center;
+  min-height: 100vh;
+  }
+  .info-main{
+    background-color: rgba(0, 0, 0, 0.739);
+    border-radius: 10px;
+    padding: 1rem;
+    color: whitesmoke;
+  }
+  .info-main-detail{
+    background-color: rgba(0, 0, 0, 0.739) !important;
+    border-radius: 10px;
+    padding: 1rem;
+  }
+  .ant-table-cell, .ant-table-cell-header {
+  text-align: center !important;
+  }
 </style>
